863fc5b9f382277d4e63953ae0bcb94c0c24d6d5
Luis Gil <giluis2000@gmail.com>
2024-10-01 21:15:55 -0400

    load wine

--- wine.py
+++ wine.py
@@ -1,1 +1,10 @@
-from sklearn.datasets import wine
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_wine
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+
+wine = load_wine()
+
+wine_df = pd.DataFrame(data=wine.data, columns=wine.feature_names)
