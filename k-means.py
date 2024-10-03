32caf8ff71fcef5c5844ac466bf6f852a83d4cce
LuchitoG <giluis2000@gmail.com>
2024-10-03 10:26:40 -0400

    puntos y centroides con clases

--- k-means.py
+++ k-means.py
@@ -3,8 +3,23 @@ import pandas as pd
 import seaborn as sns
 from sklearn.datasets import load_wine
 import matplotlib.pyplot as plt
-import math
 
+class Centroid:
+    def __init__(self, position, color) -> None:
+        self.position = position
+        self.color = color
+
+class Point:
+    def __init__(self, coordinates) -> None:
+        self.position = coordinates
+        self.centroid = None
+
+    def distance_to(self, coordinates):
+        distance = 0
+        for i in range(len(self.position)):
+            distance = (self.position[i] - coordinates[i]) ** 2 + distance
+        return np.sqrt(distance)
+        
 COLORS = ['red','blue','green','yellow','black','white','cyan','magenta','orange','purple','brown','pink','gray','olive','lime']
 
 def show_figure():
@@ -14,32 +29,28 @@ def show_figure():
 def pick_color():
     return COLORS.pop()
 
-def distance(point, centroids):
+def min_distance(point: Point, centroids: list[Centroid]):
     distances = np.zeros(len(centroids))
-    for i in range(len(point)):
-        for j in range(len(centroids)):
-            distances[j] = (point[i] - centroids[j][i]) ** 2 + distances[j]
-
-    distances = np.sqrt(distances)
+    for i in range(len(centroids)):
+        distances[i] = point.distance_to(centroids[i].position)
     return distances
 
-
 def k_means(k, points):
-    centroids_with_colors = []
-    centroids = np.random.standard_normal((k,2))
-    show_figure()
-    for centroid in centroids:
+    centroids = []
+    centroids_position = np.random.standard_normal((k,2))
+    for position in centroids_position:
         color = pick_color()
-        centroids_with_colors.append({
-            "centroid": centroid,
-            "color": color
-        }) 
-    
+        centroid = Centroid(position, color)
+        centroids.append(centroid)
+        plt.scatter(centroid.position[0], centroid.position[1], c=centroid.color, marker='x')
+
+
     for point in points:
-        distances = distance(point, centroids)
+        distances = min_distance(point, centroids)
         min_index = np.argmin(distances)
-        closest_centroid = centroids_with_colors[min_index]
-        plt.scatter(point[0], point[1], c=closest_centroid["color"])
+        closest_centroid = centroids[min_index]
+        point.centroid = closest_centroid
+        plt.scatter(point.position[0], point.position[1], c=point.centroid.color)
         
 np.random.seed(7)
 
@@ -48,10 +59,16 @@ x2 = np.random.standard_normal((100,2))*0.5-np.ones((100,2))
 x3 = np.random.standard_normal((100,2))*0.4-2*np.ones((100,2))+5
 X = np.concatenate((x1,x2,x3),axis=0)
 
+points = []
+
+for coordinates in X:
+    points.append(Point(coordinates))
+
+
 plt.plot(X[:,0],X[:,1],'k.')
 show_figure()
 
-k_means(3, X)
+k_means(3, points)
 show_figure()
 
 
