dc23bf9459de4ca76111343bbf9426df93083133
LuchitoG <giluis2000@gmail.com>
2024-10-10 08:49:31 -0400

    evaluacion de desempeno para regresion por mse

--- fish.py
+++ fish.py
@@ -3,6 +3,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import random
+from sklearn.model_selection import train_test_split
+
+def squared_error(y: list[float], y_pred: list[float]) -> float:
+    n = len(y)
+    return sum((y - y_pred) ** 2) / (2 * n)
 
 def stochastic_gradient_descent(x: list[float], y: list[float], learning_rate: float = 0.001, iterations: int = 1000):
     # Inicializamos los parámetros
@@ -41,40 +46,40 @@ def gradient_descend(x: list[float], y: list[float], learning_rate: float = 0.00
 
     for _ in range(iterations):
 
-        # Paso 1. Calcular los gradientes
-        dm = - (1/2 * n) * sum((y - m * x - b) * x)
-        db = - (1/2 * n) * sum(y - m * x - b)
+        y_pred = m * x + b
+
+        # Paso 1. Calculamos las derivadas parciales para obtener el gradiente
+        dm = (-1 / n) * sum((y - y_pred) * x)
+        db = (-1 / n) * sum(y - y_pred)
+
+        print(f'dm = {dm}, db = {db}')
 
         # Paso 2. Actualizar los parámetros
         m -= learning_rate * dm
         b -= learning_rate * db
 
         # 3. Calcular el error cuadrático medio
-        square_error = sum((y - m * x - b) ** 2) / (2 * n)
+        mse = squared_error(y, y_pred)
 
         # 4. Condición de parada basada en el error cuadrático medio
-        if square_error < tolerance:
+        if mse < tolerance:
             break
 
     return m, b
 
 
 def lineal_regression_by_min_square_errors(x: list[float], y: list[float]):
-    k1 = 0
-    k2 = 0
-    k3 = 0
-    k4 = 0
+    n = len(x)
 
     # Calculamos los valores de las constantes
-    for i in range(len(x)):
-        k1 += x[i] * y[i]
-        k2 += x[i] ** 2
-        k3 += x[i]
-        k4 += y[i]
+    k1 = sum(x * y)
+    k2 = sum(x ** 2)
+    k3 = sum(x)
+    k4 = sum(y)
     
     # Sacamos m y b
-    m = - ((k1 * len(x)) - (k3 * k4)) / (k3 ** 2 - (k2 * len(x)))
-    b = - ((k2 * k4) - (k1 * k3)) / (k3 ** 2 - k2 * len(x))
+    m = - ((k1 * n) - (k3 * k4)) / (k3 ** 2 - (k2 * n))
+    b = - ((k2 * k4) - (k1 * k3)) / (k3 ** 2 - k2 * n)
 
     return m, b
 
@@ -113,13 +118,29 @@ plt.title('Regresión Lineal por Mínimos Cuadrados')
 plt.legend()
 plt.show()
 
+# m, b = gradient_descend(X, Y, 0.001, 1000, 0.001)
 
-m, b = gradient_descend(X, Y, 1.22e-5, 1000, 0.001)
-
-print(f'm ={m}')
-print(f'b ={b}')
+# print(f'm ={m}')
+# print(f'b ={b}')
 
 # m, b = stochastic_gradient_descent(X, Y)
 
 # print(f'm ={m}')
 # print(f'b ={b}')
+
+# Dividimos la data en data de entrenamiento y data de prueba
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+# Obtenemos la pendiente y el punto de corte del modelo a partir de los datos de entrenamiento
+m_min_sq, b_min_sq = lineal_regression_by_min_square_errors(X_train, Y_train)
+
+Y_pred_min_sq = m_min_sq * X_test + b_min_sq
+
+# Calculamos el error cuadrático medio para el modelo de regresión de mínimos cuadrados
+mse_min_sq = squared_error(Y_test, Y_pred_min_sq)
+print(f'Error cuadrático medio para regresión de mínimos cuadrados: {mse_min_sq}')
+
+
+# m_grad_desc, b_grad_desc = gradient_descend(X_train, Y_train, 0.001, 1000, 0.001)
+# m_est, b_est = stochastic_gradient_descent(X_train, Y_train)
+
